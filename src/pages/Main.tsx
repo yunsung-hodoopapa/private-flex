e9bda9e2e7dc261a19d159148a9d8786fd1898e9
yunsung <yunsung@marvrus.com>
2022-12-01 22:59:04 +0900

    feat/ 이메일 발리데이션 기능 구현

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,11 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+import {
+  useState,
+  ChangeEvent,
+  useEffect,
+  SetStateAction,
+  Dispatch,
+} from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -7,8 +15,26 @@ import Input from '../components/Input';
 import PageLayout from '../components/PageLayout';
 import Section from '../components/Section';
 
+import { validators, debounce } from '../utils';
+
 export default function MainPage() {
   const { push } = useHistory();
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState<boolean | undefined>(false);
+
+  const onChangeUserMail = (e: ChangeEvent<HTMLInputElement>) => {
+    debounce(setEmail(e.target.value), 5000);
+  };
+
+  const getEmailValidation = (email: string) => {
+    const result = validators.email(email)?.includes(email);
+    return result;
+  };
+
+  useEffect(() => {
+    const bool = getEmailValidation(email);
+    setIsValid(bool);
+  }, [email]);
 
   return (
     <PageLayout>
@@ -20,8 +46,10 @@ export default function MainPage() {
         <StyledForm>
           <Input
             placeholder="이메일을 입력해주세요."
-            validationState="invalid"
-            bottomText="이메일 주소를 확인해주세요"
+            validationState={isValid ? 'valid' : 'invalid'}
+            bottomText={isValid ? '' : '이메일 주소를 확인해주세요'}
+            onChange={onChangeUserMail}
+            value={email}
           />
           <Input placeholder="비밀번호" style={{ marginTop: '16px' }} />
           <Input placeholder="비밀번호 확인" style={{ marginTop: '16px' }} />
