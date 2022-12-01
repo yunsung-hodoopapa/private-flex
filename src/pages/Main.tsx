053379f8cea385a48c4cc67c1a245eec42cc24ee
yunsung <yunsung@marvrus.com>
2022-12-01 23:54:25 +0900

    feat/비밀번호 일치여부 확인 기능 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -20,10 +20,23 @@ import { validators, debounce } from '../utils';
 export default function MainPage() {
   const { push } = useHistory();
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
   const [isValid, setIsValid] = useState<boolean | undefined>(false);
+  const [isPasswordConfirmed, setIsPasswordConfirmed] = useState<
+    boolean | undefined
+  >(false);
 
   const onChangeUserMail = (e: ChangeEvent<HTMLInputElement>) => {
     debounce(setEmail(e.target.value), 5000);
+  };
+
+  const onChnageUserPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const onChageConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
   };
 
   const getEmailValidation = (email: string) => {
@@ -31,10 +44,23 @@ export default function MainPage() {
     return result;
   };
 
+  const checkUserPassword = (password: string, confirmPassword: string) =>
+    password === confirmPassword ? true : false;
+
   useEffect(() => {
     const bool = getEmailValidation(email);
     setIsValid(bool);
   }, [email]);
+
+  useEffect(() => {
+    const bool = checkUserPassword(password, confirmPassword);
+    setIsPasswordConfirmed(bool);
+  }, [password, confirmPassword]);
+
+  console.log(password);
+  console.log(confirmPassword);
+
+  console.log(isPasswordConfirmed);
 
   return (
     <PageLayout>
@@ -51,8 +77,18 @@ export default function MainPage() {
             onChange={onChangeUserMail}
             value={email}
           />
-          <Input placeholder="비밀번호" style={{ marginTop: '16px' }} />
-          <Input placeholder="비밀번호 확인" style={{ marginTop: '16px' }} />
+          <Input
+            placeholder="비밀번호"
+            style={{ marginTop: '16px' }}
+            onChange={onChnageUserPassword}
+          />
+          <Input
+            placeholder="비밀번호 확인"
+            style={{ marginTop: '16px' }}
+            onChange={onChageConfirmPassword}
+            validationState={isPasswordConfirmed ? 'valid' : 'invalid'}
+            bottomText={isPasswordConfirmed ? '' : '비밀번호를 확인해주세요'}
+          />
         </StyledForm>
       </Section>
       <Button onClick={() => push('/skills')}>다음</Button>
