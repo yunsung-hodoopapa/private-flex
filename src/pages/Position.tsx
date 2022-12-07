e72797f6c38265bef15035d937d98bc4624297a5
yunsung <yunsung@marvrus.com>
2022-12-08 00:54:11 +0900

    feat/radio 버튼 체크 기능 구현

--- src/pages/Position.tsx
+++ src/pages/Position.tsx
@@ -1,5 +1,5 @@
 import { useHistory } from 'react-router-dom';
-
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import PageLayout from '../components/PageLayout';
@@ -10,39 +10,42 @@ import Fieldset from '../components/Fieldset';
 
 export default function PositionPage() {
   const { push } = useHistory();
+  const [selectedItem, setSelectedItem] = useState(positionWrap[0].name);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedItem(event.target.value);
+  };
+
   return (
     <PageLayout>
       <Header />
       <Section>
         <PageTitle>지원 포지션은 무엇인가요?</PageTitle>
         <Fieldset>
-          <Checkbox
-            type="radio"
-            id="frontend"
-            name="applied-position"
-            label="프론트엔드"
-          />
-          <Checkbox
-            type="radio"
-            id="backend"
-            name="applied-position"
-            label="백엔드"
-          />
-          <Checkbox
-            type="radio"
-            id="platform"
-            name="applied-position"
-            label="플랫폼"
-          />
-          <Checkbox
-            type="radio"
-            id="security"
-            name="applied-position"
-            label="보안"
-          />
+          {positionWrap.map((item, index) => {
+            const { id, name } = item;
+            return (
+              <Checkbox
+                type="radio"
+                id={id}
+                value={name}
+                name="applied-position"
+                label={name}
+                defaultChecked={index === 0}
+                onChange={onChangeHandler}
+              />
+            );
+          })}
         </Fieldset>
       </Section>
       <Button onClick={() => push('/summary')}>다음</Button>
     </PageLayout>
   );
 }
+
+const positionWrap = [
+  { id: '0', name: '프론트엔드' },
+  { id: '1', name: '백엔드' },
+  { id: '2', name: '플랫폼' },
+  { id: '3', name: '보안' },
+];
